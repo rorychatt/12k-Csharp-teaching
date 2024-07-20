cc2d44127ebdf6a47f90bd056724abc3c2ead566
rorychatt <pavel.rinne@gmail.com>
2024-07-20 23:09:22 +0200

    Feat: basic calculator logic

--- Calculator.Business/Class1.cs
+++ Calculator.Business/Class1.cs
@@ -2,5 +2,23 @@
 
 public class Class1
 {
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
 
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
 }
